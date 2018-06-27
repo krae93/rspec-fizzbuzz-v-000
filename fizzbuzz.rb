9affbe4fa602be8da7152222385817028ffa89fc
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-06-27 21:01:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
-  elsif number % 3 == 0
+  elsif num % 3 == 0
     return "Fizz"
-  elsif number % 5 == 0
+  elsif num % 5 == 0
         return "Buzz"
-    else
-      return nil
+    else 
   end
 end
